52efe0ddbb42d7d6e8977a233c885d3c53d06413
62014-51-08 <62014-51-08@exchange.ad.afpanet>
2020-03-05 16:05:00 +0100

    1ere maj active button

--- js/guillaume_projet.js
+++ js/guillaume_projet.js
@@ -4,4 +4,24 @@ $('#btnAfterbirth').click(function(e) {
     $(this).toggleClass('active');
     e.preventDefault();
 }); */
+/*boutons jeux*/
+var header = document.getElementById("btn_jeux");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
 
+/*boutons personnages*/
+var select = document.getElementById("select_persos");
+var circles = select.getElementsByClassName("cercle");
+for (var i = 0; i < circles.length; i++) {
+    circles[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
