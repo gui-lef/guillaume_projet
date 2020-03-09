284bbc6c2985b8c0795897f5aaa2366a5d26fd92
62014-51-08 <62014-51-08@exchange.ad.afpanet>
2020-03-09 16:08:59 +0100

    autre versions jeux

--- js/guillaume_projet.js
+++ js/guillaume_projet.js
@@ -1,9 +1,4 @@
-/*
-$('#btnAfterbirth').click(function(e) {
-    $('.btnAfterbirth').not(this).removeClass('active');
-    $(this).toggleClass('active');
-    e.preventDefault();
-}); */
+
 /*boutons jeux*/
 var header = document.getElementById("btn_jeux");
 var btns = header.getElementsByClassName("btn");
@@ -18,10 +13,45 @@ for (var i = 0; i < btns.length; i++) {
 /*boutons personnages*/
 var select = document.getElementById("select_persos");
 var circles = select.getElementsByClassName("cercle");
-for (var i = 0; i < circles.length; i++) {
-    circles[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
+for (var i1 = 0; i1 < circles.length; i1++) {
+    circles[i1].addEventListener("click", function() {
+        var current1 = document.getElementsByClassName(" active_cercle");
+        current1[0].className = current1[0].className.replace(" active_cercle", "");
+        this.className += " active_cercle";
+    });
+}
+
+
+/*boutons jeux Rebirth----------------------------------------------------------------------------*/
+var headerR = document.getElementById("btnR_jeux");
+var btnsR = headerR.getElementsByClassName("btnR");
+for (var ir = 0; ir < btnsR.length; ir++) {
+    btnsR[ir].addEventListener("click", function() {
+        var currentR = document.getElementsByClassName(" active");
+        currentR[0].className = currentR[0].className.replace(" active", "");
         this.className += " active";
     });
 }
+
+/*boutons personnages Rebirth
+var selectR = document.getElementById("select_persosR");
+var circlesR = selectR.getElementsByClassName("cercleR");
+for (var ir1 = 0; ir1 < circlesR.length; ir1++) {
+    circlesR[ir1].addEventListener("click", function() {
+        var currentR1 = document.getElementsByClassName(" active_cercle_R");
+        currentR1[0].className = currentR1[0].className.replace(" active_cercle_R", "");
+        this.className += " active_cercle_R";
+    });
+}
+
+
+/*boutons jeux Wotl----------------------------------------------------------------------------
+var header_W = document.getElementById("btnW_jeux");
+var btns_W = header_W.getElementsByClassName("btnW");
+for (var iw = 0; iw < btnsR.length; iw++) {
+    btns_W[iw].addEventListener("click", function() {
+        var currentR = document.getElementsByClassName(" active_W");
+        currentR[0].className = currentR[0].className.replace(" active_W", "");
+        this.className += " active_W";
+    });
+}*/
